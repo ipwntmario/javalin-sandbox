aee5d14e0c2191891a2a33f6441d7ffb934afaa3
Dylan Travers <dtravers89@gmail.com>
2025-04-13 23:16:45 -0400

    feat: both get and post verbs demonstrated

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -5,6 +5,18 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        // http://localhost:7000/ GET
+        app.get("/", ctx -> {
+            System.out.println("Hello, server!");
+            ctx.result("Hello, user!");
+        });
+
+        // http://localhost:7000/dylan POST
+        // You can't send the POST verb by visiting the URL with the browser,
+        // so you must use something like Thunder Client.
+        app.post("dylan", ctx -> {
+            ctx.result("Hello, Dylan!");
+        });
     }
 }
